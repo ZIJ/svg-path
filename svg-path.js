47f4f2bc172278271df0a6b8da4b8107044e661e
Igor Zalutsky <igor.zalutsky@gmail.com>
2014-03-06 16:19:11 +0200

    Create svg-path.js

---
+++ svg-path.js
@@ -0,0 +1,217 @@
+/**
+ * SvgPath
+ * Chainable SVG path string generator with some sugar added
+ * Supports Node, AMD and browser environments (EcmaScript 5+ or shims)
+ * No dependencies
+ * @version 0.1.0
+ * @author Igor Zalutsky
+ * @license MIT
+ */
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        define([], factory);
+    } else if (typeof exports === 'object') {
+        module.exports = factory();
+    } else {
+        root.SvgPath = factory();
+    }
+}(this, function () {
+    "use strict";
+
+    var absCommands = ['M', 'Z', 'L', 'H', 'V', 'C', 'S', 'Q', 'T', 'A'];
+    var relCommands = absCommands.map(function(letter){
+        return letter.toLowerCase();
+    });
+    var commands = absCommands.concat(relCommands);
+
+    /**
+     * Creates a path builder. Can be invoked without new.
+     * @returns {SvgPath}
+     * @constructor
+     */
+    function SvgPath(){
+        //TODO is this check robust enough?
+        if (this instanceof SvgPath){
+            this.relative = false;
+            this.commands = [];
+        } else {
+            return new SvgPath();
+        }
+    }
+
+    /**
+     * Turns relative mode on (lowercase commands will be used)
+     * @returns {SvgPath}
+     */
+    SvgPath.prototype.rel = function(){
+        this.relative = true;
+        return this;
+    };
+
+    /**
+     * Turns relative mode off (uppercase commands will be used)
+     * @returns {SvgPath}
+     */
+    SvgPath.prototype.abs = function(){
+        this.relative = false;
+        return this;
+    };
+
+    /**
+     * Closes subpath (Z command)
+     * @returns {SvgPath}
+     */
+    SvgPath.prototype.close = function(){
+        return this.Z();
+    };
+
+    /**
+     * Moves pen (M or m command)
+     * @param x
+     * @param y
+     * @returns {SvgPath}
+     */
+    SvgPath.prototype.to = function(x, y){
+        return this._cmd('M')(x, y);
+    };
+
+    /**
+     * Draws line (L or l command)
+     * @param x
+     * @param y
+     * @returns {SvgPath}
+     */
+    SvgPath.prototype.line = function(x, y){
+        return this._cmd('L')(x, y);
+    };
+
+    /**
+     * Draws horizontal line (H or h command)
+     * @param x
+     * @returns {SvgPath}
+     */
+    SvgPath.prototype.hline = function(x){
+        return this._cmd('H')(x);
+    };
+
+    /**
+     * Draws vertical line (V or v command)
+     * @param y
+     * @returns {SvgPath}
+     */
+    SvgPath.prototype.vline = function(y){
+        return this._cmd('V')(y);
+    };
+
+    /**
+     * Draws cubic bezier curve (C or c command)
+     * If x and y are omitted, acts like shortcut (S or s command)
+     * @param x1
+     * @param y1
+     * @param x2
+     * @param y2
+     * @param x
+     * @param y
+     * @returns {SvgPath}
+     */
+    SvgPath.prototype.bezier3 = function(x1, y1, x2, y2, x, y){
+        //TODO more flexible arguments handling
+        if (arguments.length >= 6) {
+            return this._cmd('C')(x1, y1, x2, y2, x, y);
+        } else {
+            return this._cmd('S')(x1, y1, x2, y2);
+        }
+    };
+
+    /**
+     * Draws quadratic bezier curve (Q or q command)
+     * If x and y are omitted, acts like shortcut (T or t command)
+     * @param x1
+     * @param y1
+     * @param x
+     * @param y
+     * @returns {SvgPath}
+     */
+    SvgPath.prototype.bezier2 = function(x1, y1, x, y){
+        //TODO more flexible arguments handling
+        if (arguments.length >= 6) {
+            return this._cmd('Q')(x1, y1, x, y);
+        } else {
+            return this._cmd('T')(x1, y1);
+        }
+    };
+
+    /**
+     * Draws an arc (A or a command)
+     * @param rx
+     * @param ry
+     * @param rotation
+     * @param large
+     * @param sweep
+     * @param x
+     * @param y
+     * @returns {*}
+     */
+    SvgPath.prototype.arc = function(rx, ry, rotation, large, sweep, x, y){
+        //TODO think of possible sugar tweaks with arcs
+        return this._cmd('A')(rx, ry, rotation, large, sweep, x, y);
+    };
+
+    /**
+     * String representation of command chain
+     * @returns {string}
+     */
+    SvgPath.prototype.str = function(){
+        return this.commands.map(function(command){
+            return command.toString();
+        }).join(' ');
+    };
+
+    //setting letter commands
+    commands.forEach(function(commandName){
+        SvgPath.prototype[commandName] = function(){
+            var args = Array.prototype.slice.call(arguments, 0);
+            args.unshift(commandName);
+            var command = new Command(args);
+            this.commands.push(command);
+            return this;
+        };
+    });
+
+    /**
+     * Gets either absolute (uppercase) or relative (lowercase) version of command depending on mode
+     * @param letter
+     * @returns {function}
+     * @private
+     */
+    SvgPath.prototype._cmd = function(letter){
+        var actualName = this.relative ?
+            letter.toLowerCase() : letter.toUpperCase();
+        //TODO maybe direct invokation is better than binding?
+        return this[actualName].bind(this);
+    };
+
+    /**
+     * Represents a single command
+     * @param name
+     * @constructor
+     */
+    function Command(name){
+        //TODO more robust array detection
+        var args = name.length > 0 && name.slice ?
+            name : Array.prototype.slice.call(arguments, 0);
+        this.name = args[0];
+        this.args = args.slice(1);
+    }
+
+    /**
+     * String representation of a command
+     * @returns {string}
+     */
+    Command.prototype.toString = function(){
+        return this.name + ' ' + this.args.join(' ');
+    };
+
+    return SvgPath;
+
+}));
